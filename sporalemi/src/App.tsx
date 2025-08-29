2a36d6c434be80004b1fa4c8bb2680012298c4d6
Furkan Uzun <126771644+brsfurkanuzun@users.noreply.github.com>
2025-08-29 18:24:01 +0300

    site added

---
+++ sporalemi/src/App.tsx
@@ -0,0 +1,14 @@
+import sporAlemiLogo from "./assets/sporAlemiLogo.svg";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <div className="mainpage">
+        <img src={sporAlemiLogo} className="logo" alt="Spor Alemi Logo" />
+      </div>
+    </>
+  );
+}
+
+export default App;
